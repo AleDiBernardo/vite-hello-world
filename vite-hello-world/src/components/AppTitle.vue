4155703226a7a638595e9c0aad37d32b8bbb3b0e
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-04-30 14:37:25 +0200

    feat: added AppTitle component

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default{
+    data(){
+        return{
+            title: "My first Vite app"
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<style>
+h1{
+    color: blue;
+}
+</style>
